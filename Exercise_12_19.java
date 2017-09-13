8c56dde6a91a0fbf1a0c281cd9c7372e947359f2
Blaine-Rob5000 <Blaine.Rob5000@stu.stech.edu>
2017-09-13 14:07:48 -0600

    Add files via upload

---
+++ Exercise_12_19.java
@@ -0,0 +1,40 @@
+//STECH Java programming class
+//Exercise 12-19
+//created by Robin G. Blaine on 9/14/2017
+
+/*
+
+12.19 (Count words) Write a program that counts the number of words in President Abraham Lincoln’s Gettysburg address from http://cs.armstrong.edu/liang/data/Lincoln.txt.
+
+Refer to page 483 for an example in Listing 12.17
+
+Remember that words are considered text that is separated by whitespace
+
+*/
+
+//imports
+import java.util.*;
+
+
+
+
+
+public class Exercise_12_19 {
+	
+	
+	
+	
+	
+	public static void main(String[] args) {
+		
+		
+		
+		
+		
+		
+	}	//end main
+	
+	
+	
+	
+}	//end Exercise_12_19
