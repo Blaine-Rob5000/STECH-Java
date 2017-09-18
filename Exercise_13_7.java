473bcba99055ae9e98b62a5e75ad4a35710b4c23
Blaine-Rob5000 <Blaine.Rob5000@stu.stech.edu>
2017-09-18 10:46:13 -0600

    Add files via upload

---
+++ Exercise_13_7.java
@@ -0,0 +1,37 @@
+//STECH Java programming class
+//Exercise 13-7
+//created by Robin G. Blaine on 9/18/2017
+
+
+/*
+
+13.7 (The Colorable interface) Design an interface named Colorable with a void method named howToColor() . Every class of a colorable object must implement the Colorableinterface. Design a class named Square that extends GeometricObject and implements Colorable . Implement howToColor to display the message Color all four sides .
+
+Draw a UML diagram that involves Colorable , Square , and GeometricObject . Write a test program that creates an array of five GeometricObject s. For each object in the array, display its area and invoke its howToColor method if it is colorable.
+
+*/
+
+
+//imports
+import java.util.*;
+
+
+public class Exercise_13_7 {
+	
+	
+	
+	
+	
+	public static void main(String[] args) {
+		
+		
+		
+		
+		
+	}	//end main
+	
+	
+	
+	
+	
+}	//end Exercise_13_7
