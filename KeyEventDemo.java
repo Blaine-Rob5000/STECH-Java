3c524057e10f6d7486402d91ff57232f6e224641
Blaine-Rob5000 <Blaine.Rob5000@stu.stech.edu>
2017-09-27 13:55:06 -0600

    Add files via upload

---
+++ KeyEventDemo.java
@@ -0,0 +1,52 @@
+// STECH Java Programming Class
+// KeyEventDemo.java
+// from Introduction to JAVA programming, Brief Version (10th Edition)
+//		Listing 15.8, pages 604-605
+// input/revised by Robin G. Blaine on 9/27/17
+
+/*
+	
+	Example of a key event handler.
+	
+*/
+
+
+//imports
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import javax.xml.stream.events.*;
+
+
+public class KeyEventDemo extends Application {
+	@Override	// Override the start method in the Application class
+	public void start(Stage primaryStage) {
+		// Create a pane and set its properties
+		Pane pane = new Pane();
+		Text text = new Text(20, 20, "A");
+		
+		pane.getChildren().add(text);
+		text.setOnKeyPressed(e -> {
+			switch (e.getCode()) {
+				case DOWN:  text.setY(text.getY() + 10); break;
+				case UP:    text.setY(text.getY() - 10); break;
+				case LEFT:  text.setX(text.getX() - 10); break;
+				case RIGHT: text.setX(text.getX() + 10); break;
+				default:
+					if (Character.isLetterOrDigit(e.getText().charAt(0)))
+						text.setText(e.getText());
+					// end if
+			}	// end switch
+		});	// end setOnKeyPressed
+		
+		// Create a scene and place it in the stage
+		Scene scene = new Scene(pane, 300, 100);
+		primaryStage.setTitle("MouseEventDemo");		// Set the stage title
+		primaryStage.setScene(scene);					// Place the scene in the stage
+		primaryStage.show();							// Display the stage
+		
+		text.requestFocus();							// text is focused to receive key input
+	}	// end start
+}	// end KeyEventDemo
