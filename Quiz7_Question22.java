f798c4927c2cd91a774652a75aa8ea291037ecbc
Blaine-Rob5000 <Blaine.Rob5000@stu.stech.edu>
2017-10-02 12:31:04 -0600

    Add files via upload

---
+++ Quiz7_Question22.java
@@ -0,0 +1,36 @@
+// STECH Java programming class
+// Module 7 Quiz, Question 22
+// created by Robin G. Blaine
+
+//imports
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.layout.Pane;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+
+
+public class Quiz7_Question22 extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		Pane pane = new Pane();
+		
+		Scene scene = new Scene(pane, 200, 200);
+		primaryStage.setTitle("Test");
+		primaryStage.setScene(scene);
+		primaryStage.show();
+		
+		pane.requestFocus();
+		
+		pane.setOnKeyPressed(e ->
+			System.out.print("Key pressed " + e.getCode() + " "));
+		pane.setOnKeyTyped(e ->
+			System.out.println("Key typed " + e.getCode()));	
+	}	// end start
+	
+	public static void main(String[] args) {
+		launch(args);
+	}	// end main
+	
+}	// end Quiz7_Question22
