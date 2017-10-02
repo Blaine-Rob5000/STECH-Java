8e906ebbb6185af8af382c712697e01506f06e51
Blaine-Rob5000 <Blaine.Rob5000@stu.stech.edu>
2017-10-02 12:03:33 -0600

    Add files via upload

--- Test.java
+++ Test.java
@@ -1,32 +1,36 @@
-//STECH Java programming class
-//Test
-//created by Robin G. Blaine
+// STECH Java programming class
+// Test
+// created by Robin G. Blaine
 
 //imports
-import java.math.*;
-import java.util.*;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.layout.Pane;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 
 
-public class Test {
-	
-	
-	
-	
+public class Test extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		Pane pane = new Pane();
+		
+		Scene scene = new Scene(pane, 200, 200);
+		primaryStage.setTitle("Test");
+		primaryStage.setScene(scene);
+		primaryStage.show();
+		
+		pane.requestFocus();
+		
+		pane.setOnKeyPressed(e ->
+			System.out.print("Key pressed " + e.getCode() + " "));
+		pane.setOnKeyTyped(e ->
+			System.out.println("Key typed " + e.getCode()));	
+	}	// end start
 	
 	public static void main(String[] args) {
+		launch(args);
+	}	// end main
 	
-	
-	
-		
-			
-	}	//end main
-	
-	
-	
-	
-	
-}	//end Test
-
-
-
-
+}	// end Test
