de75fc4bf217557f361a6514ee1895e69f0bc12d
Blaine-Rob5000 <Blaine.Rob5000@stu.stech.edu>
2017-10-03 11:21:39 -0600

    Add files via upload

---
+++ ScrollBarDemo.java
@@ -0,0 +1,53 @@
+// STECH Java programming class
+// ScrollBarDemo
+// created by Robin G. Blaine
+
+// imports
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.geometry.Orientation;
+import javafx.scene.Scene;
+import javafx.scene.control.ScrollBar;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.text.Text;
+
+
+public class ScrollBarDemo extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		Text text = new Text(20, 20, "JavaFX Programming");
+		
+		ScrollBar sbHorizontal = new ScrollBar();
+		ScrollBar sbVertical   = new ScrollBar();
+		sbVertical.setOrientation(Orientation.VERTICAL);
+		
+		Pane paneForText = new Pane();
+		paneForText.getChildren().add(text);
+		
+		BorderPane pane = new BorderPane();
+		pane.setCenter(paneForText);
+		pane.setBottom(sbHorizontal);
+		pane.setRight(sbVertical);
+		
+		sbHorizontal.valueProperty().addListener( ov ->
+			text.setX(sbHorizontal.getValue() * paneForText.getWidth() / sbHorizontal.getMax())
+		);
+		
+		sbVertical.valueProperty().addListener(ov ->
+			text.setY(sbVertical.getValue() * paneForText.getHeight() / sbVertical.getMax())
+		);
+				
+		// Create a scene and place it in the stage
+		Scene scene = new Scene(pane, 450, 170);
+		primaryStage.setTitle("ListViewDemo");
+		primaryStage.setScene(scene);
+		primaryStage.show();
+			
+	}	// end start
+	
+	public static void main(String[] args) {
+		launch(args);
+	}	// end main
+	
+}	// end ScrollBarDemo
