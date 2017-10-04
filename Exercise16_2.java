0248757f0f84220bf53d6bbea72f4fdcaae5be5d
Blaine-Rob5000 <Blaine.Rob5000@stu.stech.edu>
2017-10-04 12:39:54 -0600

    Add files via upload

---
+++ Exercise16_2.java
@@ -0,0 +1,12 @@
+// STECH Java Programming Class
+// Excercise16_2.java
+// created/modified by Robin G. Blaine on 10/4/17
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+
+	16.2 (Select geometric figures) Write a program that draws various figures, as shown in Figure  16.36b. The user selects a figure from a radio button and uses a check box to specify whether it is filled.
+		
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+
+// imports
