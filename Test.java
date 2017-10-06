981ef8557f922f4a785042097a2127caef3a44d4
Blaine-Rob5000 <Blaine.Rob5000@stu.stech.edu>
2017-10-06 10:41:55 -0600

    Add files via upload

--- Test.java
+++ Test.java
@@ -3,28 +3,29 @@
 //created by Robin G. Blaine
 
 //imports
-import java.math.*;
-import java.util.Scanner;
-import java.util.Date;
-import java.util.GregorianCalendar;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 
-
-public class Test {
-	
-	
-	
-	
+public class Test extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		StackPane pane = new StackPane();
+		Button bt1 = new Button("Java");
+		Button bt2 = new Button("Java");
+		Button bt3 = new Button("Java");
+		Button bt4 = new Button("Java");
+		pane.getChildren().addAll(bt1, bt2, bt3, bt4);
+		
+		Scene scene = new Scene(pane, 200, 250);
+		primaryStage.setTitle("Test");
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}	//end start
 	
 	public static void main(String[] args) {
-	
-	
-	
-		
-			
-	}	//end main
-	
-	
-	
-	
-	
+		launch(args);
+	}	// end main	
 }	//end Test
