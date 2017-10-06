9ccc3480fd75205374252cb0734f4c31514eb3f7
Blaine-Rob5000 <Blaine.Rob5000@stu.stech.edu>
2017-10-06 12:07:07 -0600

    Add files via upload

--- Exercise16_21.java
+++ Exercise16_21.java
@@ -3,7 +3,6 @@
 // created/modified by Robin G. Blaine on 10/5/17
 
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
-
 	16.4 (Create a miles/kilometers converter) Write a program that converts miles and kilometers, as shown in Figure  16.37b. If you enter a value in the Mile text field and press the Enter key, the corresponding kilometer measurement is displayed in the Kilometer text field. Likewise, if you enter a value in the Kilometer text field and press the Enter key, the corresponding miles is displayed in the Mile text field.
 			
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
@@ -55,28 +54,44 @@ public class Exercise16_21 extends Application {
 		};
 		
 		tfCountdown.setOnAction(e -> {
-			startTime = Integer.parseInt(tfCountdown.getText());
-			if (startTime != 0) {
-				tfCountdown.setEditable(false);
-				Timeline countdown = new Timeline(new KeyFrame(Duration.millis(1000), updateCountdown));
-				countdown.setCycleCount(startTime + 1);
-				countdown.play();
-			}	// end if
+			if (isInteger(tfCountdown.getText())) {
+				startTime = Integer.parseInt(tfCountdown.getText());
+				if (startTime >= 1) {
+					tfCountdown.setEditable(false);
+					Timeline countdown = new Timeline(new KeyFrame(Duration.millis(1000), updateCountdown));
+					countdown.setCycleCount(startTime + 1);
+					countdown.play();
+				}
+				else {
+					tfCountdown.setText("");
+				}	// end if
+			}
+			else {
+				tfCountdown.setText("");
+			}
 		});
-		
-		
-		
 				
 		// Create a scene and place it in the stage
 		Scene scene = new Scene(pane, 250, 60);
 		primaryStage.setTitle("Exercise16_21");
 		primaryStage.setScene(scene);
 		primaryStage.show();
-				
+		
 	}	// end start
+
+	
+	public boolean isInteger(String text) {
+		for (int index = 0; index < text.length(); index++) {
+			if (!Character.isDigit(text.charAt(index))) {
+				return false;		// if any character is a non-digit, return false
+			} // end if
+		}	// end for
+		return true;				// else return true
+	}	// end isInteger
+	
 	
 	public static void main(String[] args) {
 		launch(args);
-	}	// end main	
+	}	// end main
 	
 }	// end Exercise16_21
